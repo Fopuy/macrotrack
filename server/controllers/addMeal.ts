1decc8a0d712b74888efc245a1c0cff940747c24
Joshua <joshuadejesus12345@gmail.com>
2025-12-18 11:01:52 +0800

    added db

--- server/controllers/addMeal.ts
+++ server/controllers/addMeal.ts
@@ -2,6 +2,12 @@ import {prisma} from '../prisma.js'
 
 import type { Request, Response } from 'express'
 
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://nhjyqsdirnmpgvidbeeh.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
 export default async function addMeal(req:Request, res:Response){
     console.log(req.body);
     const {name, caloriesNum, proteinNum, fatNum, carbsNum } = req.body;
